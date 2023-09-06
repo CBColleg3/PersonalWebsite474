49fb26a7b1861f007b248559111b39198bf1a23e
Christopher Bennett <cbcolleg@udel.edu>
2023-09-06 16:59:55 -0400

    Added clickable header text up top.

--- index.js
+++ index.js
@@ -2,6 +2,35 @@ $( document ).ready(function() {
 
 	console.log( "ready!" );
 
-});
+	document.getElementById("AboutMeLink").onclick = () => {
+		console.log("click is running! Lets Go");
+		document.getElementById("Home").style.display = "none";
+		document.getElementById("AboutMe").style.display = "";
+		document.getElementById("Contacts").style.display = "none";
+		document.getElementById("WorkHistory").style.display = "none";
+	}
 
-$(document)
+	document.getElementById("HomePage").onclick = () => {
+		console.log("click is running! Lets Go");
+		document.getElementById("Home").style.display = "";
+		document.getElementById("AboutMe").style.display = "none";
+		document.getElementById("Contacts").style.display = "none";
+		document.getElementById("WorkHistory").style.display = "none";
+	}
+
+	document.getElementById("ContactsPage").onclick = () => {
+		console.log("click is running! Lets Go");
+		document.getElementById("Home").style.display = "none";
+		document.getElementById("AboutMe").style.display = "none";
+		document.getElementById("Contacts").style.display = "";
+		document.getElementById("WorkHistory").style.display = "none";
+	}
+
+	document.getElementById("WorkHistoryPage").onclick = () => {
+		console.log("click is running! Lets Go");
+		document.getElementById("Home").style.display = "none";
+		document.getElementById("AboutMe").style.display = "none";
+		document.getElementById("Contacts").style.display = "none";
+		document.getElementById("WorkHistory").style.display = "";
+	}
+});
